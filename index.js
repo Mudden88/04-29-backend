6d62df2998a75a951722355f0bcc1299548e83ff
Mudden88 <mudden@live.se>
2024-04-29 09:14:40 +0200

    Added 2nd version

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express(),
   port = process.env.PORT || 3000
 
 app.get('/', (_request, response) => {
-  response.send({ hello: 'World' })
+  response.status(200).send({ hello: 'World' })
 })
 
 app.listen(port, () => {
